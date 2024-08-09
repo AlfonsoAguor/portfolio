f57614539e959efc1e8c0506c0c9c7865de87940
AlfonsoAguor <alfonsoaguor@gmail.com>
2024-08-09 12:02:42 +0200

    Commit
    Actualizacion con nuevo proyecto, cambios en el css y js.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -77,3 +77,29 @@ document.getElementById('copyText').addEventListener('click', function() {
         confirmationMessage.style.display = 'none';
     }, 2000);
 });
+
+/* Funcion del carousel*/
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else {
+        currentIndex = index;
+    }
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+setInterval(nextSlide, 5000);
